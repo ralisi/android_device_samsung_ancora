a3e9be6584f23c49fc9ac1ee156444d061f9ddc1
Raphael Lisicki <raphael@ralisi.de>
2013-10-12 12:07:42 -0300

    delted all references to prebuilt kernel-files

--- ancora.mk
+++ ancora.mk
@@ -142,23 +142,6 @@ PRODUCT_PACKAGES += \
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.secure=0
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-    LOCAL_KERNEL := $(LOCAL_PATH)/prebuilt/zImage
-    # prebuilt kernel modules
-    PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/prebuilt/cifs.ko:root/lib/modules/cifs.ko \
-        $(LOCAL_PATH)/prebuilt/dhd.ko:root/lib/modules/dhd.ko \
-        $(LOCAL_PATH)/prebuilt/scsi_wait_scan.ko:root/lib/modules/scsi_wait_scan.ko \
-        $(LOCAL_PATH)/prebuilt/cifs.ko:system/lib/modules/cifs.ko \
-        $(LOCAL_PATH)/prebuilt/dhd.ko:system/lib/modules/dhd.ko \
-        $(LOCAL_PATH)/prebuilt/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko
-else
-    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
 # We have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
